3f2c4b96a0cf8facd1e19215aa62ddf2ca234d81
Huy Phan Luong <phanhuy04062003@gmail.com>
2024-10-30 22:36:50 +0700

    add Redis for cache get Songs

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,22 @@ import playlistRoutes from "./routes/playlistRoutes.js";
 import { getSongs, streamSong } from "./controllers/songController.js";
 import { userJwtMiddleware } from "./middlewares/authMiddleware.js";
 
+import { createClient } from 'redis';
+
+const redisClient = createClient({
+  url: 'redis://redis:6379'
+});
+
+redisClient.connect()
+  .then(() => console.log('Connected to Redis'))
+  .catch(err => console.error('Redis connection error:', err));
+
+
 // Load environment variables
 dotenv.config();
 
 // Initialize Express app
-const app = express();
+const app = express();;
 
 // Middleware
 app.use(express.json());
@@ -38,7 +49,27 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/song", userJwtMiddleware, songRoutes);
 app.use("/api/v1/playlist", userJwtMiddleware, playlistRoutes);
 app.get("/api/v1/stream/:filename", streamSong);
-app.get('/api/v1/songs', getSongs);
+
+
+// app.get('/api/v1/songs', getSongs);
+app.get('/api/v1/songs', async (req, res) => {
+  try {
+    const cachedSongs = await redisClient.get('songs');
+    
+    if (cachedSongs) {
+      return res.json(JSON.parse(cachedSongs));
+    }
+
+    const songs = await getSongs();
+    await redisClient.set('songs', JSON.stringify(songs), { EX: 3600 }); // Cache for 1 hour
+
+    res.json(songs);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving songs" });
+  }
+});
+
 
 // Fallback to index.html for SPA
 app.get("*", (req, res) => {
